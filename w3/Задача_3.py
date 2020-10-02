9e555d9529ce7afaa5cb25d85d0e8c552844f22b
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-02 10:53:40 +0300

    Update Задача_3.py

--- w3/Задача_3.py
+++ w3/Задача_3.py
@@ -1,11 +1,11 @@
 import zipfile
-fantasy_zip = zipfile.ZipFile('This PC\\Downloads\\main.zip')
-fantasy_zip.extractall('This PC\\Downloads')
-fantasy_zip.close()
+mane_zip = zipfile.ZipFile('C:\\Users\\main.zip')
+mane_zip.extractall('C:\\Downloads\\Users')
+mane_zip.close()
 
 import os
 
-with open('luffy.txt', 'a') as f:
+with open('result.txt', 'a') as f:
     for current_dir, dirs, files in os.walk('main'):
         if list(filter(lambda x: x.endswith('.py'), files)):
             f.write('{}\n'.format(current_dir))
