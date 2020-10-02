5017e4ec1e514aa1689d912ac0d1604e04ca74a9
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-02 10:39:06 +0300

    Update Задача_2.py

--- w3/Задача_2.py
+++ w3/Задача_2.py
@@ -2,4 +2,4 @@ def write_array(array, file_name):
     array = map(lambda x: x + '\n',array)
     with open(file_name, "w") as file:
          file.writelines(array)
-write_array(["i","love","you"],"2.txt")
+write_array(["sunset","verry","beautiful!"],"2.txt")
