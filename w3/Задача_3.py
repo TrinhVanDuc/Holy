2bf4bfb42e03278ae93641674dd91ea7a7db4c01
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-02 14:00:03 +0300

    Update Задача_3.py

--- w3/Задача_3.py
+++ w3/Задача_3.py
@@ -9,3 +9,5 @@ with open('result.txt', 'a') as f:
     for current_dir, dirs, files in os.walk('main'):
         if list(filter(lambda x: x.endswith('.py'), files)):
             f.write('{}\n'.format(current_dir))
+
+#Я не могу запустить эту программу на своем компьютере, поэтому просто записываю команды -__-
