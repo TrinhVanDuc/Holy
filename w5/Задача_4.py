5494ed7236663faa97fc637e1ec2e4c3f13ca1ee
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-13 21:50:24 +0300

    Update Задача_4.py

--- w5/Задача_4.py
+++ w5/Задача_4.py
@@ -1,29 +1,17 @@
 class Shape:
-    _area = -1
-
-    def __init__(self, area, length):
-        self._area = area
-        self._length = length
-
-    def get_area(self):
-        raise NotImplementedError()
-
-    def __int__(self, width, height):
+    def __init__(self,width, height):
         self.width = width
         self.height = height
-    def area(self):
-        print('Area is:',self.get_area())
 
 class Triangle(Shape):
-    
-    def __init__(self, side1, side2):
-        self.side1 = side1
-        self.side2 = side2
-        
-    def get_area(self):
-        if self._area == -1:
-            self._area = (self.side1)*(self.side2)
-        return self._area
+    def area(self):
+        return (self.width)*(self.height)*0.5
 
+class Rectangle(Shape):
+    def area(self):
+        return (self.width)*(self.height)
+    
 tr1 = Triangle(4,3)
-tr1.area()
+print(tr1.area())   #6.0
+tr2 = Rectangle (4,3)
+print(tr2.area())   #12
