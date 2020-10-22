8b7381280a440c3e87c0e0934296e7fd8044f211
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-22 15:58:32 +0300

    Update hero.py

--- arithmetic_dragons/hero.py
+++ arithmetic_dragons/hero.py
@@ -2,14 +2,14 @@
 # license: GPLv3
 from gameunit import *
 class Hero(Attacker):
-    def __init__(self, name):
+    def __init__(self,name):
         self._health = 100
         self._attack = 50
-        self._espenriense = 0
-    def attack(self, target):
-        target._health -=  self._attack
-        Hero += 1
+        self._experience = 0
+    def attack(self,target):
+        target._health -= self._attack
+        Hero._experience += 1
     def gameOver(self):
-        return self._health <= 0
+        return self._health <= 0 
 
 
