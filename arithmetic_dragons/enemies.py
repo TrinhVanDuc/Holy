589422054851ed40220164eac5820f33f4d17175
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-22 15:54:49 +0300

    Update enemies.py

--- arithmetic_dragons/enemies.py
+++ arithmetic_dragons/enemies.py
@@ -12,6 +12,7 @@ def generate_random_enemy():
     enemy = RandomEnemyType()
     return enemy
 
+
 def generate_dragon_list(enemy_number):
     enemy_list = [generate_random_enemy() for i in range(enemy_number)]
     return enemy_list
@@ -37,33 +38,32 @@ class GreenDragon(Dragon):
         self.__quest = str(x) + '+' + str(y)
         self.set_answer(x + y)
         return self.__quest
+
 class RedDragon(Dragon):
     def __init__(self):
         self._health = 200
         self._attack = 10
         self._color = 'красный'
-        
+
     def question(self):
         x = randint(1,100)
         y = randint(1,100)
         self.__quest = str(x) + '-' + str(y)
-        self.set_answer(x-y)
+        self.set_answer(x - y)
         return self.__quest
-
+        
 class BlackDragon(Dragon):
     def __init__(self):
         self._health = 200
         self._attack = 10
-        self._color = 'чёрный'
-        
+        self._color = 'черный'
+
     def question(self):
         x = randint(1,100)
         y = randint(1,100)
-        self.__quest = srt(x) + '*' + str(y)
-        self.set_answer(x*y)
-        return self.__quest
-
-
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x * y)
+        return self.__quest        
 
 
 enemy_types = [GreenDragon, RedDragon, BlackDragon]
