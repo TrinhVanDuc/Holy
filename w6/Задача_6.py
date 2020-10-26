934ac3acba69827477121016e0e0b18b472ea643
TrinhVanDuc <71319943+TrinhVanDuc@users.noreply.github.com>
2020-10-26 22:22:19 +0300

    Update Задача_6.py

--- w6/Задача_6.py
+++ w6/Задача_6.py
@@ -1,56 +1,31 @@
-import math
-import cmath
+class ComlexCalculate:
+    def __init__(self,x,y):
+        self.real = x
+        self.imag = y
+    def __str__(self):
+        return  f'({self.real},{self.imag}j)'
+    def __add__(self, other):
+        return ComlexCalculate(self.real + other.real,self.imag - other.imag)
+    def __sub__(self, other):
+        return ComlexCalculate(self.real - other.real,self.imag - other.imag)
+    def __mul__(self, other):
+        return ComlexCalculate(self.real*other.real-self.imag*other.imag, self.real*other.imag+self.imag*other.real)
+    def __truediv__(self, other):
+        return ComlexCalculate((self.real*other.real+self.imag*other.imag)/(other.real**2+other.imag**2),
+                               (-self.real * other.imag + self.imag * other.real)/(other.real**2+other.imag**2))
+    def __neg__(self):
+        return ComlexCalculate(-self.real,-self.imag)
+    def __abs__(self):
+        return ((self.real**2+self.imag**2)**0.5)
 
-class MyMath:
-    pi = 3.14
-    _complex = False # Инкапсуляция
-
-    @staticmethod
-    def sin(x):
-        return math.sin(x)
-
-    @classmethod
-    def complex(cls):
-        return cls._complex
-
-    @classmethod
-    def sqrt(cls, x):
-        if x < 0:
-            if cls.complex() == False: # Полимофизм
-                raise ValueError('This is an error')
-            else:
-                y = cmath.sqrt(x)
-                return (y.real, y.imag)
-        else:
-            return math.sqrt(x)
-
-class MyComlexMath(MyMath): # Наследоаоние
-    _complex = True        # Инкапсуляция и Наследование
-        
-    
-if __name__ == "__main__":
-    My = MyMath()
-    Mycompl = MyComlexMath()
-    
-    #возвращающий синус числа (число задано в радианах)
-    print('Sin(1) = {}\nSin(pi) = {}\n'.format(My.sin(1),My.sin(MyMath.pi),))
-
-    #класс MyComplexMath не падает и считает корень из отрицательного числа при вызове sqrt от отрицательного числа
-    print('Square root of 8 = {}\nSquare root of -8 = {} '.format(My.sqrt(8),Mycompl.sqrt(-8)))
-
-    #класс MyMath падает при вызове метода sqrt от отрицательного числа
-    try:
-        c = My.sqrt(-8)
-        print(c)
-    except Exception:
-        print('There was an error!')
-'''
-Мы использовали классовый метод, а не статический потому что, 
-классовый метод может получить доступ или изменить состояние класса, а статический метод - нет.
-'''
-
-
-
-
-
+if __name__ == '__main__':
+    x = ComlexCalculate(1,1)
+    y = ComlexCalculate(2,2)
+    print(x,y)
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print('-x =',ComlexCalculate.__neg__(x))
+    print('abs(x) =',abs(x))
 
