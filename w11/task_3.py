b415d90ba3e202f905f9fe3fc0c55e9c2eedd437
Đức Trịnh <ducphuc0311@gmail.com>
2020-12-07 02:28:45 +0300

    add news files in w12

--- w11/task_3.py
+++ w11/task_3.py
@@ -1,22 +1,21 @@
+import aiohttp
 import asyncio
 import aiofile
-import aiohttp
-import re
 
-async def fetch_html(url, session):
-    async with session.request(method="GET", url=url) as resp:
-        html = await resp.text()
-        html = re.split(r'[\n]', html)
+async def func(url, session, outfi):
+    async with session.get(url) as response:
+        html = await response.text()
         for i in html:
-            if i.startswith("<a >"):
-                async with aiofile.async_open('found.txt', 'w') as f:
-                    f.write(i)
+            if i.startswith('a'):
+                await outfi.write(str(i) + '\n')
 
-async def parse(url):
-    async with aiofile.async_open(url, 'r') as f:
-        async for line in aiofile.LineReader(f):
+async def main(in_file, out_file):
+    with open(in_file, 'r') as f:
+        urls = [line.strip() for line in f.readline()]
+    async with aiofile.async_open(out_file, 'a') as outfi:
             async with aiohttp.ClientSession() as session:
-                asyncio.ensure_future(fetch_html(line, session))
+                task = [asyncio.create_task(func(url, session, outfi)) for url in urls]
+            await asyncio.gather(*task)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(parse('urls.txt'))
+loop.run_until_complete(main('urls.txt', 'found.txt'))
